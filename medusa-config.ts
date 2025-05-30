8576f70dcd3bb2e77c79e7ba95dc80a6d214b2ac
lakshay choudhary <lakshaychoudhary77712@gmail.com>
2025-05-30 12:14:06 +0530

    add S3 file storage module and updating allowed hosts to use environment variable for production domain

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,7 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
+import { Modules } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
@@ -11,12 +12,36 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
   admin: {
-    vite: config => {
-      config.server.allowedHosts = ['YOUR-DOMAIN']
-      return config
-    }
-  }
-})
+    vite: (config) => {
+      config.server.allowedHosts = [process.env.NEXT_PUBLIC_PROD_DOMAIN!];
+      return config;
+    },
+  },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+              additional_client_config: {
+                forcePathStyle: true,
+              },
+            },
+          },
+        ],
+      },
+    },
+  ],
+});
